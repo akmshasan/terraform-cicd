d37551e5496300b4d4d5e1e596a735f1adcc5c49
A K M Shafiul Hasan <akmshasan@gmail.com>
2023-02-18 13:00:11 +0800

    Updated provider

--- provider.tf
+++ provider.tf
@@ -1,4 +1,14 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.53.1"
+    }
+  }
+}
+
 provider "google" {
+  # Configuration options
   project     = "learning-0165559734"
   region      = "asia-southeast1"
 }
